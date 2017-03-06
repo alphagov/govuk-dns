7e2094350d34b6ba21e37efb3a5e753c9ecfa115
Laura Martin <surminus@gmail.com>
2017-03-06 12:48:09 +0000

    Update Jenkins argument
    Jenkins will call either "plan" or "apply". Update the script so it runs
    the relevant task.

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
 set -e
+
+case "$1" in
+  plan)
+    CMD=plan
+    ;;
+  apply)
+    CMD=apply
+    ;;
+  *)
+    echo "Didn't recognise argument: must be plan or apply"
+    exit 1
+esac
+
 export ZONEFILE=publishing.service.gov.uk.yaml
 
 git clone 'git@github.gds:gds/govuk-dns-config.git'
@@ -9,4 +22,4 @@ cp govuk-dns-config/$ZONEFILE .
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake generate
-bundle exec rake plan
+bundle exec rake ${CMD}
